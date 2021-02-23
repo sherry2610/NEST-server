fd8334c9795fa4700fdc39ddf09f0e7a95983ac6
sherry2610 <shaharyarkhalid2610@gmail.com>
2021-02-23 14:27:40 +0500

    feat: create product module and link it with AppModule

---
+++ src/products/products.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { ProductsController } from "./products.controller";
+import { ProductsServices } from "./products.services";
+
+@Module({
+  controllers:[ProductsController],
+  providers: [ProductsServices]
+})
+export class ProductModule {}
